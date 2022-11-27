4a437b06ea967743f781db732d337eb12d7354a7
Ashley <ashleynjeri7@gmail.com>
2022-11-27 19:25:52 +0300

    Passed Tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # GET /plants
+    #get '/plants', to:'plants#index'
+  resources :plants, only:[:index, :show, :create]
+
+   # GET /plants/:id
+  #get ''
+
+  
 end
